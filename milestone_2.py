0708392f487f705196d8ab8a6c2bd6bf93be722d
ANCA JUVERDEANU <119536039+Juveanca13@users.noreply.github.com>
2023-12-07 17:46:21 +0000

    Update milestone_2.py

--- milestone_2.py
+++ milestone_2.py
@@ -1,18 +1,36 @@
-import random
+def get_user_guess():
+    """
+    Prompt the user to enter a single letter and validate the input.
+    Returns:
+        str: The user's valid input.
+    """
+    while True:
+        guess = input("Enter a single letter: ")
+        if len(guess) == 1 and guess.isalpha():
+            return guess
+        else:
+            print("Oops! That is not a valid input. Please enter a single letter.")
 
+def print_validation_result(valid_input):
+    """
+    Print the validation result based on the provided input.
+    Args:
+        valid_input (bool): Whether the input is valid or not.
+    """
+    if valid_input:
+        print("Good guess!")
+    else:
+        print("Oops! That is not a valid input.")
+
+# Existing code from Milestone 1
 favorite_fruits = ["Apple", "Banana", "Orange", "Grapes", "Mango"]
-
 word_list = favorite_fruits
 
-word = random.choice(word_list)
+# Print out the newly created list to the standard output (screen).
+print("Word List:", word_list)
 
-print("Randomly Selected Word:", word)
+# Step 1: Get user input and validate
+user_guess = get_user_guess()
 
-guess = input ("Enter a single letter: ")
-
-print ("Your guess:", guess)
-
-if len(guess) == 1 and guess.isalpha():
-    print("Good guess!")
-else:
-    print("Oops! That is not a valid input.")
+# Step 2: Print validation result
+print_validation_result(user_guess.isalpha() and len(user_guess) == 1)
