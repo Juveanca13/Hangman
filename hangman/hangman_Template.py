efb0610ee467ab9d4cf25067daf47922bc852030
ANCA JUVERDEANU <119536039+Juveanca13@users.noreply.github.com>
2023-12-07 00:20:41 +0000

    Update hangman_Template.py

--- hangman/hangman_Template.py
+++ hangman/hangman_Template.py
@@ -41,10 +41,10 @@ class Hangman:
         Asks the user for a letter.
     '''
     def __init__(self, word_list, num_lives=5):
-        # TODO 2: Initialize the attributes as indicated in the docstring
-        # TODO 2: Print two message upon initialization:
-        # 1. "The mistery word has {num_letters} characters"
-        # 2. {word_guessed}
+        self.word_list = word_list
+        self.num_lives = num_lives
+        self.list_of_guesses = []
+        self._choose_word()
         pass
 
     def check_letter(self, letter) -> None:
